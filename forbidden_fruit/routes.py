70294d2808ce2fbc78c9a0e4c61da433ca09eb81
AmerikanischerAdler <114196294+AmerikanischerAdler@users.noreply.github.com>
2024-08-02 20:10:11 -0400

    updated routes.py added url_for

--- forbidden_fruit/routes.py
+++ forbidden_fruit/routes.py
@@ -1,10 +1,11 @@
 from forbidden_fruit.main import app
-from flask import render_template
+from flask import render_template url_for
 
-@app.route("/")
+@app.route("/home")
 def home():
     return render_template("index.html")
 
 @app.route("/labs")
 def labs():
     return render_template("labs.html")
+
