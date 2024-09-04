2d5a988225f2dfe99eda343fc7fdc3d41193060f
AmerikanischerAdler <114196294+AmerikanischerAdler@users.noreply.github.com>
2024-09-04 16:33:00 -0400

    updated flask structure and added 404 page

--- forbidden_fruit/routes.py
+++ forbidden_fruit/routes.py
@@ -1,6 +1,7 @@
 from forbidden_fruit.main import app
 from flask import render_template, url_for
 
+@app.route("/")
 @app.route("/home")
 def home():
     return render_template("index.html")
@@ -8,4 +9,8 @@ def home():
 @app.route("/labs")
 def labs():
     return render_template("labs.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
